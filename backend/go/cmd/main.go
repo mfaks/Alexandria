56a215b1f3b71ffe53267cae8b163099fa5699cf
mfaks <muhammad.faks@gmail.com>
2024-08-25 13:51:30 -0400

    created local db connection

--- backend/go/cmd/main.go
+++ backend/go/cmd/main.go
@@ -1,1 +1,62 @@
 package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Failed to load .env file")
+	}
+
+	mysqlConnectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DATABASE"))
+
+	mysqlDB, err := sql.Open("mysql", mysqlConnectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer mysqlDB.Close()
+
+	err = mysqlDB.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MySQL")
+
+	mongoConnectionString := fmt.Sprintf("mongodb://%s:%s@%s:%s/%s",
+		os.Getenv("MONGO_USER"),
+		os.Getenv("MONGO_PASSWORD"),
+		os.Getenv("MONGO_HOST"),
+		os.Getenv("MONGO_PORT"),
+		os.Getenv("MONGO_DATABASE"))
+
+	clientOptions := options.Client().ApplyURI(mongoConnectionString)
+	mongoClient, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer mongoClient.Disconnect(context.TODO())
+	err = mongoClient.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
+
+}
