3f8a255b9749f8f81894ea028625279b8dd31862
mfaks <muhammad.faks@gmail.com>
2024-08-26 17:49:05 -0400

    setup oath with google and discord

--- backend/go/auth/main.go
+++ backend/go/auth/main.go
@@ -3,18 +3,30 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/github"
 	"golang.org/x/oauth2/google"
 )
 
 var (
+	githubOauthConfig *oauth2.Config
+	googleOauthConfig *oauth2.Config
+)
+
+func init() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	githubOauthConfig = &oauth2.Config{
 		ClientID:     os.Getenv("GITHUB_CLIENT_ID"),
 		ClientSecret: os.Getenv("GITHUB_CLIENT_SECRET"),
@@ -30,7 +42,7 @@ var (
 		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
 		Endpoint:     google.Endpoint,
 	}
-)
+}
 
 func main() {
 	r := chi.NewRouter()
@@ -116,7 +128,6 @@ func handleCallback(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to decode user info", http.StatusInternalServerError)
 		return
 	}
-	defer response.Body.Close()
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(userInfo)
