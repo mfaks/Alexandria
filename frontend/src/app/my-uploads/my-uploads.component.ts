be812048c3073f4de58063a7861982d8cf55f06a
mfaks <muhammad.faks@gmail.com>
2024-08-31 21:38:59 -0400

    created my uploads component

---
+++ frontend/src/app/my-uploads/my-uploads.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-uploads',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-uploads.component.html',
+  styleUrl: './my-uploads.component.css'
+})
+export class MyUploadsComponent {
+
+}
