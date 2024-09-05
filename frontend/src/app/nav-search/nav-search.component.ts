7d9a114fe8c0cd5a2d0db4d2b98e44d37f1c31bf
mfaks <muhammad.faks@gmail.com>
2024-09-05 19:26:23 -0400

    added auth to nav bar search

--- frontend/src/app/nav-search/nav-search.component.ts
+++ frontend/src/app/nav-search/nav-search.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-nav-search',
@@ -9,8 +10,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './nav-search.component.html',
   styleUrl: './nav-search.component.css',
 })
-export class NavSearchComponent {
+export class NavSearchComponent implements OnInit {
   searchText: string = '';
+  userInfo: any;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.userInfo = this.authService.getUserInfo();
+    this.authService.fetchUserInfo().subscribe(
+      (userInfo) => {
+        this.userInfo = userInfo;
+      },
+      (error) => {
+        console.error('Error fetching user info:', error);
+      }
+    );
+  }
 
   clearSearch(): void {
     this.searchText = '';
