dee7171cfa5c0c33dbc8452f0cb0e165fa7c9391
mfaks <muhammad.faks@gmail.com>
2024-09-18 16:24:23 -0700

    feat: added semantic chat search navbar

---
+++ frontend/src/app/search.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Document {
+  _id: string;
+  title: string;
+  authors: string[];
+  description?: string;
+  categories: string[];
+  fileName: string;
+  thumbnailUrl: string;
+  lastUpdated: string;
+  isPublic: boolean;
+  user_email: string;
+  similarity_score?: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  private apiUrl = 'http://localhost:8000';
+
+  constructor(private http: HttpClient) { }
+
+  searchDocuments(query: string, topK: number = 5): Observable<Document[]> {
+    return this.http.post<Document[]>(`${this.apiUrl}/search_documents/`, { query, top_k: topK }, { withCredentials: true });
+  }
+}
