e250cee04e74e289534e13523468764607a28460
mfaks <muhammad.faks@gmail.com>
2024-09-20 18:28:09 -0700

    feat: added contact us dialog

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactDialogComponent } from '../contact-dialog/contact-dialog.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ContactDialogComponent],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  isContactDialogOpen = false;
+
   scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth'});
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  openContactDialog() {
+    this.isContactDialogOpen = true;
+  }
+
+  closeContactDialog() {
+    this.isContactDialogOpen = false;
   }
 }
